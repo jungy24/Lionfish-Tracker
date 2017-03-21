6b6e988b58769d9c765b3cc4864ae1e658ceb95b
Jungyoon Yu <yu612@purdue.edu>
2017-03-21 18:54:35 -0400

    Segeues working

--- Lionfish/ViewController.swift
+++ Lionfish/ViewController.swift
@@ -9,12 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    @IBOutlet weak var backView: UIImageView!
-    
-    
-    
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
